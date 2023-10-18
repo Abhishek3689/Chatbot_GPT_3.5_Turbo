99147627ada69e8851524c611e067e8ecb755511
Abhishek3689 <abhishek.nishad3689@gmail.com>
2023-10-18 12:13:18 +0530

    updated python file

---
+++ Research/tele_bot_demo.py
@@ -0,0 +1,25 @@
+import logging
+import os
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+load_dotenv()
+api_token=os.getenv("TOKEN")
+#print(api_token)
+
+#configure loggin
+logging.basicConfig(level=logging.INFO,format='%(asctime)s %(message)s')
+
+## Initialize bot and dispathcer
+bot=Bot(token=api_token)
+dp=Dispatcher(bot)
+
+@dp.message_handler(commands=['start','help'])
+async def command_start_handler(message: types.Message):
+    """
+    This handler receives messages with `/start` command
+    """
+    
+    await message.reply(f"Hello! \nI am Echobot \npowered by Abhishek")
+
+if __name__ == "__main__":
+    executor.start_polling(dp,skip_updates=True)

e9a1d12e0885dad354b829825f6a802e223b7def
Abhishek3689 <abhishek.nishad3689@gmail.com>
2023-10-21 10:30:53 +0530

    Chatgpt 3.5 model added

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,13 +6,13 @@ from aiogram import Bot,Dispatcher,executor,types
 
 class Reference:
     '''
-     A class to store previously input text 
+     A class to store previously response from chat gpt 3.5
     '''
     def __init__(self):
         self.response=""
 
 load_dotenv()
-#openai.api_key=os.getenv("OpenAI_API_KEY")
+openai.api_key=os.getenv("OpenAI_API_KEY")
 Token=os.getenv("TOKEN")
 reference=Reference()
 
@@ -51,13 +51,29 @@ async def menu_option(message:types.Message):
     '''
     menu_list="""
     Hi!\n I am telebot created by Abhishek \n\nChoose from below menu option\n
-    * start : To start the Conversation
-    * clear : To clear previous chat history
-    * help  : To go to Main Menu
-    * menu  : To go to Menu option
+    */start : To start the Conversation
+    */clear : To clear previous chat history
+    */help  : To go to Main Menu
+    */menu  : To go to Menu option
     """
     await message.reply(menu_list)
 
+@dispatcher.message_handler()
+async def chat_gpt(message:types.Message):
+    '''
+    A handler to process the user input and generate response using chatgpt API
+    '''
+    print(f">>>User \n\t{message.text}")
+    response=openai.ChatCompletion.create(
+        model=model_name,
+        messages=[
+            {"role": "assistant", "content": reference.response}, # role assistant
+            {"role": "user", "content": message.text} #our query 
+        ])
+
+    reference.response = response['choices'][0]['message']['content']
+    print(f">>>Chatgpt response\n\t:{reference.response}")
+    await bot.send_message(chat_id=message.chat.id,text=reference.response)
 
 if __name__ == "__main__":
     executor.start_polling(dispatcher, skip_updates=True)
